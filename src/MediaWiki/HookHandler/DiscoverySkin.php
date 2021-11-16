027a2b96e27a60b0cae037b6c5ada7680b148ef7
pwirth <wirth@hallowelt.biz>
2021-11-16 15:49:48 +0100

    Fix hard requirement for BlueSpiceDiscovery skin
    ERM:26482
    [4.x]
    
    Change-Id: Ia429a7757f6a4dc35ca85dd1bd2c06848513b09a

---
+++ src/MediaWiki/HookHandler/DiscoverySkin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MediaWiki\Extension\Workflows\MediaWiki\HookHandler;
+
+use BlueSpice\Discovery\Hook\BlueSpiceDiscoveryTemplateDataProviderAfterInit;
+use BlueSpice\Discovery\ITemplateDataProvider;
+
+class DiscoverySkin implements BlueSpiceDiscoveryTemplateDataProviderAfterInit {
+
+	/**
+	 *
+	 * @param ITemplateDataProvider $registry
+	 * @return void
+	 */
+	public function onBlueSpiceDiscoveryTemplateDataProviderAfterInit( $registry ): void {
+		$registry->register( 'actions_secondary', 'ca-wf_start' );
+		$registry->unregister( 'toolbox', 'ca-wf_start' );
+	}
+}
