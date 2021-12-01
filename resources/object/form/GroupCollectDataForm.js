d101ffadbe946a1af0be3f9d387f0bc4c4a3e897
akulbii <alk@astarios.com>
2021-12-01 09:16:00 +0200

    Move messages and diagrams to workflows extension
    As soon as "UserFeedback" and "GroupFeedback" are based only on Workflows activities - they are moved to Workflows extension with corresponding translated messages.
    
    [4.1.x]
    
    ERM26534
    
    Change-Id: Iee76673d3c5b6ff02c2354ac002bd0e3e931fba4

---
+++ resources/object/form/GroupCollectDataForm.js
@@ -0,0 +1,29 @@
+( function ( mw, $ ) {
+	workflows.object.form.GroupCollectData = function( cfg, activity ) {
+		workflows.object.form.GroupCollectData.parent.call( this, cfg, activity );
+	};
+
+	OO.inheritClass( workflows.object.form.GroupCollectData, workflows.object.form.Form );
+
+	workflows.object.form.GroupCollectData.prototype.getDefinitionItems = function() {
+		return [
+			{
+				name: 'groupname',
+				label: mw.message( 'workflows-collect-data-form-groupname' ).text(),
+				type: 'group_picker',
+				required: true
+			},
+			{
+				name: 'instructions',
+				label: mw.message( 'workflows-collect-data-form-instructions' ).text(),
+				type: 'wikitext'
+			},
+			{
+				name: 'reportrecipient',
+				label: mw.message( 'workflows-collect-data-form-reportrecipient' ).text(),
+				type: 'text'
+			}
+		];
+	};
+
+} )( mediaWiki, jQuery );
