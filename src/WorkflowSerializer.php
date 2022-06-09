8f3d971c4d751fa5de4b904f41ddea81467d21ac
miriam schlindwein <schlindwein@hallowelt.com>
2022-06-09 15:32:03 +0200

    Improve styling grid list
    ERM27798
    
    [4.2.x]
    
    Change-Id: Ibcf262a0b8e7fc402d0ae5c67e1c8fe4255952d8

--- src/WorkflowSerializer.php
+++ src/WorkflowSerializer.php
@@ -123,8 +123,10 @@ class WorkflowSerializer {
 			'start' => $last->dateTime()->format( 'YmdHis' ),
 			// Unfortunate mix, but found no good way to format client-side
 			'startFormatted' => $lang->userTimeAndDate( $start->dateTime()->format( 'YmdHis' ), $user ),
+			'startDate' => $lang->userDate( $start->dateTime()->format( 'YmdHis' ), $user ),
 			'last' => $last->dateTime()->format( 'YmdHis' ),
 			'lastFormatted' => $lang->userTimeAndDate( $last->dateTime()->format( 'YmdHis' ), $user ),
+			'lastDate' => $lang->userDate( $last->dateTime()->format( 'YmdHis' ), $user ),
 		];
 	}
 }
