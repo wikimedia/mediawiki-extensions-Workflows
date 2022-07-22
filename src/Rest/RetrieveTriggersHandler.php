d91885697004d01228cb7d85891bd4a52aee103e
rvogel <vogel@hallowelt.biz>
2022-07-22 17:14:39 +0200

    Fix "manual triggers" for subpages
    Passing slashes into the `{page}` param of the
    `/workflow/triggers/manual/` REST route will cause the router to fail.
    
    We can not safely use URL-encoding, as the webserver may decode it
    automatically.
    
    Now the endpoint is changed to allow passing pagename as QS param
    
    ERM29304
    
    [4.2.x]
    
    Change-Id: I938683ab6b9767d2ce24a89f602a0f589d3c2f51

--- src/Rest/RetrieveTriggersHandler.php
+++ src/Rest/RetrieveTriggersHandler.php
@@ -2,35 +2,101 @@
 
 namespace MediaWiki\Extension\Workflows\Rest;
 
+use MediaWiki\Extension\Workflows\Trigger\PageRelatedTrigger;
+use MediaWiki\Extension\Workflows\TriggerRepo;
 use MediaWiki\Rest\HttpException;
+use Title;
+use TitleFactory;
 use Wikimedia\ParamValidator\ParamValidator;
 
 class RetrieveTriggersHandler extends TriggerHandler {
+	/** @var TitleFactory */
+	private $titleFactory;
+	/** @var bool */
+	private $listForType;
+
+	public function __construct(
+		TriggerRepo $triggerRepo, ?TitleFactory $titleFactory = null, bool $listForType = false
+	) {
+		parent::__construct( $triggerRepo );
+		$this->titleFactory = $titleFactory;
+		$this->listForType = $listForType;
+	}
+
 	/**
 	 * @inheritDoc
 	 */
 	public function execute() {
-		$triggers = $this->getTriggerRepo()->getAll();
-
 		$params = $this->getValidatedParams();
-		if ( $params['key'] === '*' ) {
+		$key = $params['key'];
+		if ( $this->listForType ) {
+			return $this->getResponseFactory()->createJson(
+				$this->getTriggersOfType( $params['page'], $key )
+			);
+		}
+
+		$triggers = $this->getTriggerRepo()->getAll();
+		if ( $key === '*' ) {
 			return $this->getResponseFactory()->createJson( $triggers );
 		}
-		if ( !isset( $triggers[$params['key'] ] ) ) {
-			throw new HttpException( 'Trigger ' . $params['key'] . ' not found', 404 );
+
+		if ( !isset( $triggers[$key] ) ) {
+			throw new HttpException( "Trigger $key not found", 404 );
 		}
-		return $this->getResponseFactory()->createJson( $triggers[$params['key']]->jsonSerialize() );
+		return $this->getResponseFactory()->createJson( $triggers[$key]->jsonSerialize() );
 	}
 
 	/**
 	 * @inheritDoc
 	 */
 	public function getParamSettings() {
-		return [
+		$params = [
 			'key' => [
 				static::PARAM_SOURCE => 'path',
 				ParamValidator::PARAM_REQUIRED => true,
 			]
 		];
+		if ( $this->listForType ) {
+			$params['page'] = [
+				static::PARAM_SOURCE => 'query',
+				ParamValidator::PARAM_REQUIRED => false,
+			];
+		}
+
+		return $params;
+	}
+
+	/**
+	 * @param string|null $page
+	 * @param string $key
+	 *
+	 * @return array
+	 */
+	private function getTriggersOfType( ?string $page, string $key ): array {
+		$triggers = $this->getTriggerRepo()->getAllOfType( $key );
+
+		if ( $page ) {
+			$title = $this->titleFactory->newFromDBkey( $page );
+			if ( !( $title instanceof Title ) || !$title->exists() ) {
+				return [];
+			}
+
+			$allowed = [];
+			foreach ( $triggers as $trigger ) {
+				if ( !( $trigger instanceof PageRelatedTrigger ) ) {
+					continue;
+				}
+				if ( !$trigger->isActive() ) {
+					continue;
+				}
+				$trigger->setTitle( $title );
+				if ( $trigger->shouldTrigger() ) {
+					$allowed[$trigger->getId()] = $trigger;
+				}
+			}
+			return $allowed;
+		}
+
+		return $triggers;
 	}
 }
