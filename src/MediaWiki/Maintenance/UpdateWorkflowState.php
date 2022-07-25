46af3863722042a839f6329a1d8d5dbffc24d505
akulbii <alk@astarios.com>
2022-07-25 12:13:48 +0300

    Add logged maintenance script to rebuild workflows state data
    Recently, additional data is stored as the workflow "state".
    On update, old records need to be updated.
    This script rebuils all the data by replaying all workflow events.
    So current solution is to just run this script once for each system, to fix corrupted records.
    
    [4.2.x]
    
    ERM29150
    
    Change-Id: I9179e88dba9e129d1797f1bb38542a0f5789224f

---
+++ src/MediaWiki/Maintenance/UpdateWorkflowState.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MediaWiki\Extension\Workflows\MediaWiki\Maintenance;
+
+use LoggedUpdateMaintenance;
+use MediaWiki\Extension\Workflows\Query\WorkflowStateStore;
+use MediaWiki\Extension\Workflows\Storage\WorkflowEventRepository;
+use MediaWiki\MediaWikiServices;
+
+class UpdateWorkflowState extends LoggedUpdateMaintenance {
+	/**
+	 * @inheritDoc
+	 */
+	protected function doDBUpdates() {
+		/** @var WorkflowEventRepository $eventStore */
+		$eventStore = MediaWikiServices::getInstance()->getService( 'WorkflowEventRepository' );
+		/** @var WorkflowStateStore $stateStore */
+		$stateStore = MediaWikiServices::getInstance()->getService( 'WorkflowsStateStore' );
+
+		$eventStore->addReplayConsumer( $stateStore );
+
+		$ids = $eventStore->retrieveAllIds();
+		$this->output(
+			'...Update workflows state store: Found ' . count( $ids ) . ' workflow(s)...'
+		);
+		foreach ( $ids as $id ) {
+			// Retrieving will recreate WorkflowStorage from events...
+			// ... and fire each event for out state store to react to
+			$eventStore->retrieve( $id );
+		}
+		$this->output( "done\n" );
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function getUpdateKey() {
+		return 'workflow-state-store-update';
+	}
+}
