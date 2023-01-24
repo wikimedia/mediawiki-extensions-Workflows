60bfeae058248620f0d4bb4b1036b02211a78c9e
d.savuljesku <d.savuljesku@gmail.com>
2023-01-24 11:31:59 +0100

    Fix popup dimentions in WF details dialog
    Also, prevent number of users blowing up the dialog when a group with many users
    is assigned
    
    [4.2.x]
    
    ERM 28882
    
    Change-Id: I73d88166cc777f57488b4e5b01a03576cb4b6111
    (cherry picked from commit ba902089d36202c5ae60e7eb6eed235b0985b01d)

--- resources/ui/widget/ActivityRawDataPopup.js
+++ resources/ui/widget/ActivityRawDataPopup.js
@@ -12,38 +12,47 @@
 			};
 		}
 
-		cfg.popup = {
-			$content: this.getContent(),
-			padded: true,
+		var popupContent = this.getContent();
+		cfg.popup = $.extend( cfg.popup || {}, {
+			$content: popupContent.$content,
+			height: popupContent.height + 30,
+			width: popupContent.width + 30,
+			padded: false,
+			autoFlip: false,
 			classes: [ 'workflow-data-popup' ]
-		};
+		} );
 
 		workflows.ui.widget.ActivityRawDataPopup.parent.call( this, cfg );
-
+		this.popup.connect( this, {
+			ready: function() {
+				// this hack is needed because popup body is never set to the correct height,
+				// even though it should, based on docu. This is a workaround for that.
+				// Has to be in an event handler as its lazy set
+				this.popup.$body.css( 'height', '100%' );
+			}
+		} );
 		this.$element.children()[1].tabIndex = 0;
-		this.$element.addClass( this.getPopupClass() );
+		this.$element.addClass( 'activity-data-raw-popup' );
 	};
 
 	OO.inheritClass( workflows.ui.widget.ActivityRawDataPopup, OO.ui.PopupButtonWidget );
 
-	workflows.ui.widget.ActivityRawDataPopup.prototype.getPopupClass = function() {
-		return 'activity-data-raw-popup';
-	}
-
-	workflows.ui.widget.ActivityRawDataPopup.prototype.getTableClass = function() {
-		return 'activity-data-table';
-	}
-
 	workflows.ui.widget.ActivityRawDataPopup.prototype.getContent = function() {
-		var $table = $( '<table>' ).addClass( this.getTableClass() ).append(
-			$( '<tr class="popup-table-header">' ).append(
-				$( '<th>' ).text( mw.message( 'workflows-ui-overview-details-raw-data-popup-prop' ).text() ),
-				$( '<th>' ).text( mw.message( 'workflows-ui-overview-details-raw-data-popup-value' ).text() )
-			)
-		);
+		var $table = $( '<table>' )
+			.addClass( 'activity-data-table' )
+			.append(
+				$( '<tr class="popup-table-header">' ).append(
+					$( '<th>' ).text( mw.message( 'workflows-ui-overview-details-raw-data-popup-prop' ).text() ),
+					$( '<th>' ).text( mw.message( 'workflows-ui-overview-details-raw-data-popup-value' ).text() )
+				)
+			);
 
 		for ( var prop in this.data ) {
 			if ( !this.data.hasOwnProperty( prop ) ) {
+				continue;
+			}
+			if ( !this.data[prop] ) {
+				// Do not show empty values
 				continue;
 			}
 			$table.append(
@@ -54,6 +63,15 @@
 			);
 		}
 
-		return $table;
-	}
+		// Hack to get dimensions of the table (has to be in the DOM)
+		$( 'body' ).append( $table );
+		var height = $table.outerHeight(),
+			width = $table.outerWidth();
+		$table.remove();
+		return {
+			$content: $table,
+			width: width,
+			height: height
+		};
+	};
 } )( mediaWiki, jQuery );
