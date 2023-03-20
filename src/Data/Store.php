b3fb24d37ed2a5a703a47b7b5d20fff1549f21c5
Hamish Slater <hamishslater@hotmail.com>
2023-03-20 12:29:39 +0100

    Fix deprecated calls to 'User::newFromName'
    ERM29677
    
    [REL1_39, master]
    
    Change-Id: I45ae220b00326a2358282d9d56caaf448f2248a3

--- src/Data/Store.php
+++ src/Data/Store.php
@@ -5,6 +5,7 @@ namespace MediaWiki\Extension\Workflows\Data;
 use MediaWiki\Extension\Workflows\Query\WorkflowStateStore;
 use MediaWiki\Extension\Workflows\WorkflowFactory;
 use MediaWiki\Linker\LinkRenderer;
+use MediaWiki\User\UserFactory;
 use MWStake\MediaWiki\Component\DataStore\IStore;
 
 class Store implements IStore {
@@ -16,21 +17,25 @@ class Store implements IStore {
 	private $titleFactory;
 	/** @var LinkRenderer */
 	private $linkRenderer;
+	/** @var UserFactory */
+	private $userFactory;
 
 	/**
 	 * @param WorkflowStateStore $stateStore
 	 * @param WorkflowFactory $wfFactory
 	 * @param \TitleFactory $titleFactory
 	 * @param LinkRenderer $linkRenderer
+	 * @param UserFactory $userFactory
 	 */
 	public function __construct(
 		WorkflowStateStore $stateStore, WorkflowFactory $wfFactory,
-		\TitleFactory $titleFactory, LinkRenderer $linkRenderer
+		\TitleFactory $titleFactory, LinkRenderer $linkRenderer, UserFactory $userFactory
 	) {
 		$this->stateStore = $stateStore;
 		$this->workflowFactory = $wfFactory;
 		$this->titleFactory = $titleFactory;
 		$this->linkRenderer = $linkRenderer;
+		$this->userFactory = $userFactory;
 	}
 
 	/**
@@ -46,7 +51,7 @@ class Store implements IStore {
 	public function getReader() {
 		return new Reader(
 			$this->stateStore, $this->workflowFactory,
-			$this->titleFactory, $this->linkRenderer
+			$this->titleFactory, $this->linkRenderer, $this->userFactory
 		);
 	}
 }
