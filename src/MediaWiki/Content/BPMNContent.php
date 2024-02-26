b8739170cfc0a902ab2ab820cc2365bf4d90f9ac
Daniel Riedmüller <daniel.riedmueller@hallowelt.com>
2024-02-26 14:05:44 +0100

    Validate xml before save
    ERM32122
    
    [4.4.x]
    
    Change-Id: I152fb47ffeb2d8556ce5d19216a501bfb4cb096c

--- src/MediaWiki/Content/BPMNContent.php
+++ src/MediaWiki/Content/BPMNContent.php
@@ -8,4 +8,16 @@ class BPMNContent extends TextContent {
 	public function __construct( $text ) {
 		parent::__construct( $text, 'BPMN' );
 	}
+
+	public function isValid() {
+		if ( empty( $this->getText() ) ) {
+			return true;
+		}
+
+		if ( simplexml_load_string( $this->getText() ) === false ) {
+			return false;
+		}
+
+		return true;
+	}
 }
