dec1074362b7fe1f95380ce051f353a43b712f4e
d.savuljesku <d.savuljesku@gmail.com>
2024-08-30 15:46:45 +0200

    Adapt to renaming of Notifications
    [4.5.x]
    
    Depends on https://gitlab.hallowelt.com/HalloWelt/mediawiki-extensions-Notifications/-/merge_requests/37
    
    ERM38462
    
    Change-Id: Iac70a2306a83d88172a22ca31508ce92ebab7149
    (cherry picked from commit b1c26b91d48e89e72e3ba2ae90c4cc18bd568e3a)
    (cherry picked from commit c0cdd50ba97ed13160d9914692e266cfd4b7fafb)

--- src/Event/WorkflowAbortedEvent.php
+++ src/Event/WorkflowAbortedEvent.php
@@ -6,10 +6,11 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\User\UserIdentity;
 use Message;
 use MWStake\MediaWiki\Component\Events\Delivery\IChannel;
+use MWStake\MediaWiki\Component\Events\PriorityEvent;
 use MWStake\MediaWiki\Component\Events\TitleEvent;
 use Title;
 
-class WorkflowAbortedEvent extends TitleEvent {
+class WorkflowAbortedEvent extends TitleEvent implements PriorityEvent {
 
 	/** @var string */
 	private $reason;
