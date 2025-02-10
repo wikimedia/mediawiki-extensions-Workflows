4b9dfe86b837412f38da13066e3d688fad85cc82
hslater <hamish.slater@hallowelt.com>
2025-02-10 21:33:13 +0100

    Update BeforePageDisplayHook namespace
    ERM38444
    
    [REL1_43, master]

--- src/MediaWiki/Hook/AddBootstrap.php
+++ src/MediaWiki/Hook/AddBootstrap.php
@@ -2,7 +2,7 @@
 
 namespace MediaWiki\Extension\Workflows\MediaWiki\Hook;
 
-use MediaWiki\Hook\BeforePageDisplayHook;
+use MediaWiki\Output\Hook\BeforePageDisplayHook;
 
 class AddBootstrap implements BeforePageDisplayHook {
 
