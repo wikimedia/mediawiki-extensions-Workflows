5bb40c68f7aef2d285bf751f10e777f8920fe88c
Hamish <hamish.slater@hallowelt.com>
2026-04-22 20:42:46 +0700

    Add foreign-resources.yaml for SBOM tracking of third-party resources
    ERM47272
    
    [5.1.x]
    
    Change-Id: I9dd63b09ab67c936b227e06f46a0050c7696e982
    (cherry picked from commit e062b9ff14b79ec2ca615a0da9fc5c6e485577ce)

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function ( grunt ) {
 				'**/*.{css,less}',
 				'!node_modules/**',
 				'!vendor/**',
-				'!resources/ui/editor/bpmn-js/**'
+				'!resources/lib/**'
 			]
 		},
 		banana: Object.assign(
