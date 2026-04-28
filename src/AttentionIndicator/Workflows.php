7f468e529c7f48b556d4ebe09de7b48cb88645ad
d.savuljesku <d.savuljesku@gmail.com>
2026-04-28 10:27:09 +0200

    Fix AttentionIndicator counter
    Align more to actual method that will provide UTO descriptors
    
    Yes, this is worse performance-wise, but necessary, as otherwise, count is just a guess
    
    [5.1.x]
    
    ERM47044
    
    Change-Id: If3338e7c379a47630a80e27e0d6c97136f56335f
    (cherry picked from commit a0e75ce5512545e85990ae0c2932f91d4cdcc648)

--- src/AttentionIndicator/Workflows.php
+++ src/AttentionIndicator/Workflows.php
@@ -5,13 +5,17 @@ namespace MediaWiki\Extension\Workflows\AttentionIndicator;
 use BlueSpice\Discovery\AttentionIndicator;
 use BlueSpice\Discovery\IAttentionIndicator;
 use MediaWiki\Config\Config;
+use MediaWiki\Extension\Workflows\Definition\ITask;
 use MediaWiki\Extension\Workflows\Query\WorkflowStateStore;
 use MediaWiki\Extension\Workflows\Storage\Event\TaskIntermediateStateChanged;
 use MediaWiki\Extension\Workflows\Storage\Event\TaskLoopCompleted;
 use MediaWiki\Extension\Workflows\Storage\Event\TaskStarted;
+use MediaWiki\Extension\Workflows\UserInteractiveActivity;
+use MediaWiki\Extension\Workflows\WorkflowFactory;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\User\User;
 use MediaWiki\User\UserFactory;
+use Throwable;
 
 class Workflows extends AttentionIndicator {
 
@@ -26,18 +30,25 @@ class Workflows extends AttentionIndicator {
 	protected $userFactory;
 
 	/**
+	 * @var WorkflowFactory
+	 */
+	protected $workflowFactory;
+
+	/**
 	 * @param string $key
 	 * @param Config $config
 	 * @param User $user
 	 * @param WorkflowStateStore $stateStore
 	 * @param UserFactory $userFactory
+	 * @param WorkflowFactory $workflowFactory
 	 */
 	public function __construct(
 		string $key, Config $config, User $user,
-		WorkflowStateStore $stateStore, UserFactory $userFactory
+		WorkflowStateStore $stateStore, UserFactory $userFactory, WorkflowFactory $workflowFactory
 	) {
 		$this->stateStore = $stateStore;
 		$this->userFactory = $userFactory;
+		$this->workflowFactory = $workflowFactory;
 		parent::__construct( $key, $config, $user );
 	}
 
@@ -48,12 +59,13 @@ class Workflows extends AttentionIndicator {
 	 * @param MediaWikiServices $services
 	 * @param WorkflowStateStore|null $stateStore
 	 * @param UserFactory|null $userFactory
-	 *
+	 * @param WorkflowFactory|null $workflowFactory
 	 * @return IAttentionIndicator
 	 */
 	public static function factory(
 		string $key, Config $config, User $user, MediaWikiServices $services,
-		?WorkflowStateStore $stateStore = null, ?UserFactory $userFactory = null
+		?WorkflowStateStore $stateStore = null, ?UserFactory $userFactory = null,
+		?WorkflowFactory $workflowFactory = null
 	) {
 		if ( !$stateStore ) {
 			$stateStore = $services->getService( 'WorkflowsStateStore' );
@@ -61,13 +73,17 @@ class Workflows extends AttentionIndicator {
 		if ( !$userFactory ) {
 			$userFactory = $services->getUserFactory();
 		}
+		if ( !$workflowFactory ) {
+			$workflowFactory = $services->getService( 'WorkflowFactory' );
+		}
 
 		return new static(
 			$key,
 			$config,
 			$user,
 			$stateStore,
-			$userFactory
+			$userFactory,
+			$workflowFactory
 		);
 	}
 
@@ -91,14 +107,34 @@ class Workflows extends AttentionIndicator {
 		$assignedActivitiesCount = 0;
 		$models = $this->stateStore->modelsFromIds( $ids );
 		foreach ( $models as $model ) {
+			$validTask = false;
 			foreach ( $model->getAssignees() as $assigneeName ) {
 				$assignedUser = $this->userFactory->newFromName( $assigneeName );
 				if ( $assignedUser === null ) {
 					continue;
 				}
 				if ( $assignedUser->getId() === $this->user->getId() ) {
-					$assignedActivitiesCount++;
+					try {
+						$workflow = $this->workflowFactory->getWorkflow( $model->getWorkflowId() );
+						$current = $workflow->current();
+						foreach ( $current as $item ) {
+							if ( !$item instanceof ITask || $item->getElementName() !== 'userTask' ) {
+								continue;
+							}
+							$activity = $workflow->getActivityForTask( $item );
+							if ( !$activity instanceof UserInteractiveActivity ) {
+								continue;
+							}
+							$validTask = true;
+						}
+					} catch ( Throwable ) {
+						// NOOP - skip
+					}
 				}
+			}
+
+			if ( $validTask ) {
+				$assignedActivitiesCount++;
 			}
 		}
 
